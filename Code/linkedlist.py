baa8f6719afac5cb6b7c8c54ccdd7ef84059f941
Luke Parker <lukeaparker@gmail.com>
2020-02-14 20:59:42 -0800

    added time complexities

--- Code/linkedlist.py
+++ Code/linkedlist.py
@@ -73,8 +73,8 @@ class LinkedList(object):
     def get_at_index(self, index):
         """Return the item at the given index in this linked list, or
         raise ValueError if the given index is out of range of the list size.
-        Best case running time: ??? under what conditions? [TODO]
-        Worst case running time: ??? under what conditions? [TODO]"""
+        Best case running time: O(1) under what conditions? [if item is located at head]
+        Worst case running time: O(n) under what conditions? [If item is not located at head]"""
         # Check if the given index is out of range and if so raise an error
         if not (0 <= index < self.size):
             raise ValueError('List index out of range: {}'.format(index))
@@ -87,8 +87,8 @@ class LinkedList(object):
     def insert_at_index(self, index, item):
         """Insert the given item at the given index in this linked list, or
         raise ValueError if the given index is out of range of the list size.
-        Best case running time: ??? under what conditions? [TODO]
-        Worst case running time: ??? under what conditions? [TODO]"""
+        Best case running time: o(1) under what conditions? [If you want to insert at head]
+        Worst case running time: o(n) under what conditions? [If you insert anywhere other than at head]"""
         # Check if the given index is out of range and if so raise an error
         
         node = self.head 
@@ -111,7 +111,7 @@ class LinkedList(object):
 
     def append(self, item):
         """Insert the given item at the tail of this linked list.
-        Best and worst case running time: ??? under what conditions? [TODO]"""
+        Best and worst case running time: o(n) always when your appending to tail? """
         # Create a new node to hold the given item
         new_node = Node(item)
         # Check if this linked list is empty
@@ -162,8 +162,8 @@ class LinkedList(object):
     def replace(self, old_item, new_item):
         """Replace the given old_item in this linked list with given new_item
         using the same node, or raise ValueError if old_item is not found.
-        Best case running time: ??? under what conditions? [TODO]
-        Worst case running time: ??? under what conditions? [TODO]"""
+        Best case running time: o(1) if quality is at head
+        Worst case running time: o(n) if quality is not at head"""
         node = self.head
         while node is not None:
             if node.data == old_item:
@@ -186,8 +186,8 @@ class LinkedList(object):
 
     def delete(self, item):
         """Delete the given item from this linked list, or raise ValueError.
-        Best case running time: ??? under what conditions? [TODO]
-        Worst case running time: ??? under what conditions? [TODO]"""
+        Best case running time: o(1) if item is at head
+        Worst case running time: o(n) if item is not at head"""
         # Start at the head node
         node = self.head
         # Keep track of the node before the one containing the given item

8892a475a646401454098b393ae2ff8355701e8e
Luke Parker <lukeaparker@gmail.com>
2020-03-07 18:11:18 -0800

    added anograms

--- Code/word_jumble.py
+++ Code/word_jumble.py
@@ -2,8 +2,13 @@ import os
 
 class Jumble:
 
-    def __init__(self):
+    def __init__(self, first_word, second_word, third_word, fourth_word):
         self.dict = self._group()
+        self.first_word = first_word
+        self.second_word = second_word
+        self.third_word = third_word
+        self.fourth_word = fourth_word
+        self.first_answer = self.generate_first_answer(first_word, second_word, third_word, fourth_word)
 
     def _generate_lookup(self):
         with open("/usr/share/dict/words") as f:
@@ -43,7 +48,7 @@ class Jumble:
                 ano_dict.setdefault(key, [word])
         return ano_dict
     
-    def find_ano(self, key_word):
+    def _find_ano(self, key_word):
         key = self._generate_id(key_word)
         possible_choices = self.dict.get(key)
         choice_list = []
@@ -52,19 +57,26 @@ class Jumble:
                 if word != key_word:
                     choice_list.append(word)
         return choice_list
-
-
     
-
-
-def test():
-    jumble = Jumble()
-    print(jumble.find_ano('prouot'))
+    def generate_first_answer(self, first_word, second_word, third_word, fourth_word):
+        first_word = self._find_ano(self.first_word[0])
+        second_word = self._find_ano(self.second_word[0])
+        third_word = self._find_ano(self.third_word[0])
+        fourth_word = self._find_ano(self.second_word[0])
+        print(first_word)
+        print(second_word)
+        print(third_word)
+        print(fourth_word)
     
+def run():
+    first_word = ('laisa', [1, 2, 3])
+    second_word = ('laurr', [0, 2])
+    third_word = ('bureek', [0, 1])
+    fourth_word = ('prouot', [2, 4, 5])
+    jumble = Jumble(first_word, second_word, third_word, fourth_word)
+    print(jumble.first_answer)
     
-
-
-test()
+run()
 
 
 
